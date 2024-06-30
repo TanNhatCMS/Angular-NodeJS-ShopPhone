c81440fb459d1c8c9561670c7852be0977055c27
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-30 11:46:20 +0700

    Delete global-error-handler.ts

--- frontend/src/app/global-error-handler.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorHandler, Injectable } from '@angular/core';
-
-@Injectable()
-export class GlobalErrorHandler implements ErrorHandler {
-  handleError(error: any): void {
-    // Log the error to a monitoring service or console
-    console.error('Global Error:', error);
-  }
-}

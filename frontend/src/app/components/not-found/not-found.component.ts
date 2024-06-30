0f51dadd6ad4a657018cd11758dd3791af62792b
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-30 11:45:08 +0700

    add component not fund

---
+++ frontend/src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-foun',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+
+}

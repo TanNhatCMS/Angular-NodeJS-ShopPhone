7f2def105a2f980574695963ce60e90cdc25499b
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-06-30 11:46:09 +0700

    update component

---
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Product } from '../../models/products';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+  productDetail:Product|undefined;
+  constructor(private router: ActivatedRoute, private prod: ProductsService){
+
+  }
+  ngOnInit(): void {
+    let id = Number(this.router.snapshot.paramMap.get('id'));
+    this.productDetail = this.prod.getProductById(id);
+  }
+}

971e926d98919db3860fac3e21e7cc49681f5951
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-07-02 14:35:50 +0700

    add api products

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/products';
 import { ProductsService } from '../../services/products.service';
+import {forkJoin, mergeMap, of, switchMap} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   products: Product[] = [];
-  constructor(prod: ProductsService) {
-    this.products = prod.getAllProducts();
+
+  constructor(private productsService: ProductsService) {
+    //this.products = prod.getAllProducts();
   }
+  ngOnInit(): void {
+    this.filterProductList = this.products;
+    this.productsService.currentProducts.subscribe((res: Array<any>) => {
+      const [data] = res;
+      if (data && data.length) {
+        this.products = data;
+      } else {
+         this.products = [];
+         this.getProducts();
+      }
+    });
+
+  }
+  getProducts(): void {
+    this.productsService
+      .getAllProducts()
+      .subscribe((data: any) => {
+        console.log( data);
+        this.products = data;
+        this.productsService.changeProducts([data]||[]);
+      });
+  }
+  //
+  // getColor = (num: number) =>
+  //   num < 50 ? 'text-danger' : num > 1500 ? 'text-success' : 'text-warning';
   filterProductList: Product[] = [];
   searching: string = '';
 
-  ngOnInit(): void {
-    this.filterProductList = this.products;
-  }
+
 
   filterResults(): void {
     this.filterProductList = this.searching ?

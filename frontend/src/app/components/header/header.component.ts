93a2e5024796ddd5b5ea28430d904f8b2e12fb1f
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-07-05 15:16:22 +0700

    Update header.component.ts

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,20 @@ import {AuthService} from "../../services/auth.service";
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  constructor(private authService: AuthService) { }
+  @Input() dark: boolean=false;
+  isdark: boolean = true;
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if(this.dark) {
+      const threshold = 100;
+      this.isdark = window.scrollY > threshold;
+    }else {
+      this.isdark = true;
+    }
+  }
+  constructor(private authService: AuthService) {
+    this.isdark = !this.dark;
+  }
   isAuthenticated() {
     return this.authService.isAuthenticated
   }

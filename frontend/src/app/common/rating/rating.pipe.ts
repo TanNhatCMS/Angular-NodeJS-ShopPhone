647d3995327ad56d1a4cbc01fd8ea2df5d3d529d
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-07-05 15:16:49 +0700

    Create rating.pipe.ts

---
+++ frontend/src/app/common/rating/rating.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  standalone: true,
+  name: 'formatRating'
+})
+export class RatingPipe implements PipeTransform {
+
+  transform(value: any): any {
+    const result = Math.floor(value / 2);
+    return result;
+  }
+
+}

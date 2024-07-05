e7ff0b77c3654443223aa1ccc20af6f33587fc08
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-07-05 15:15:49 +0700

    Update products.service.ts

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -15,73 +15,7 @@ export class ProductsService {
     this.apiUrl = environment.apiUrl;
   }
 
-  products: Product[] = [
-    {
-      id: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2016",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "/assets/images/Leaf-Rake.png",
-      code: "some-code-1",
-      des: "some-description-1",
-      inStock: 10
-    },
-    {
-      id: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2016",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "/assets/images/garden-cart.png",
-      code: "some-code-2",
-      des: "some-description-2",
-      inStock: 20
-    },
-    {
-      id: 5,
-      productName: "Hammer",
-      productCode: "TBX-0048",
-      releaseDate: "May 21, 2016",
-      description: "Curved claw steel hammer",
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: "/assets/images/rejon-Hammer.png",
-      code: "some-code-3",
-      des: "some-description-3",
-      inStock: 30
-    },
-    {
-      id: 8,
-      productName: "Saw",
-      productCode: "TBX-0022",
-      releaseDate: "May 15, 2016",
-      description: "15-inch steel blade hand saw",
-      price: 11.55,
-      starRating: 3.7,
-      imageUrl: "/assets/images/egore911-saw.png",
-      code: "some-code-4",
-      des: "some-description-4",
-      inStock: 40
-    },
-    {
-      id: 10,
-      productName: "Video Game Controller",
-      productCode: "GMG-0042",
-      releaseDate: "October 15, 2015",
-      description: "Standard two-button video game controller",
-      price: 35.95,
-      starRating: 4.6,
-      imageUrl: "/assets/images/xbox-controller.png",
-      code: "some-code-5",
-      des: "some-description-5",
-      inStock: 50
-    }
-  ];
+  products: Product[] = [];
   getAllProducts = (): Observable<any> =>
     this.http.get<any>(`${this.apiUrl}/products`);
 

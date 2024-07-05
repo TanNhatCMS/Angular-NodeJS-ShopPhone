a9522e59a57275dd658fae838d77439da8ba74c8
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-07-05 15:16:03 +0700

    Update products.ts

--- frontend/src/app/models/products.ts
+++ frontend/src/app/models/products.ts
@@ -1,14 +1,70 @@
+export type Products = Product[];
+
+export interface ProductRatings {
+  averageRating: number;
+}
+
+export interface ProductTechnicalInfoContent {
+  name: string;
+  description: string;
+}
+
+export interface ProductTechnicalInfo {
+  title: string;
+  content: ProductTechnicalInfoContent[];
+}
+
+export interface ProductVariantsImages {
+  mainImage: string;
+  imageGallery: string[];
+}
+
+export interface ProductVariants {
+  images: ProductVariantsImages;
+  productStatus: string;
+  color: string;
+  ram: string;
+  storage: string;
+  price: number;
+  promotionalPrice: number;
+  promotionStatus: boolean;
+  stockQuantity: number;
+}
+
 export interface Product {
-    id: number;
-    productName: string;
-    productCode: string;
-    releaseDate: string;
-    description: string;
-    price: number;
-    starRating: number;
-    imageUrl: string;
-    code: string;
-    des: string;
-    inStock: number;
-  }
-  
+  id: number;
+  productName: string;
+  productCode: string;
+  releaseDate: string;
+  description: string;
+
+  starRating: number;
+  imageUrl: string;
+  code: string;
+  des: string;
+  inStock: number;
+
+  thumbnail: string;
+  ratings: ProductRatings;
+  _id: string;
+  technicalInfo: ProductTechnicalInfo[];
+  variants: ProductVariants[];
+  screen: string;
+  backCamera: string;
+  frontCamera: string;
+  brand: string;
+  category: string;
+  owner: string;
+  name: string;
+  shortDescription: string;
+  detailedDescription: string;
+  productStatus: string;
+  color: string;
+  ram: string;
+  storage: string;
+  price: number;
+  promotionalPrice: number;
+  promotionStatus: boolean;
+  stockQuantity: number;
+  images: [];
+}
